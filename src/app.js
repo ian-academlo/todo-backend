430e0414cc8cfbdc50bfce3da5ad45eefad30bdb
ianAcademlo <ian.rosas@academlo.com>
2022-10-12 17:59:04 -0500

    moving cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 var cors = require("cors");
 const userRoutes = require("./routes/task.routes");
-app.use(cors());
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/todo/", userRoutes);
