cd8e8f979d02e7322c3e20c40c3eac0c26104366
ianAcademlo <ian.rosas@academlo.com>
2022-10-12 17:51:19 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+var cors = require("cors");
 const userRoutes = require("./routes/task.routes");
-
+app.use(cors());
 const app = express();
 
 app.use(express.json());
