d12f62a77d78bcaf1653abed3fabb2d5ad4594c3
ianAcademlo <ian.rosas@academlo.com>
2022-10-12 19:07:12 -0500

    modified dialect

--- src/config/config.js
+++ src/config/config.js
@@ -20,6 +20,7 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOSTNAME,
+    dialect: "postgres",
     dialectOptions: { ssl: { require: true, rejectUnauthorized: false } },
   },
 };
